cc04f7883601b902869077410c3878f1a3f42dd6
adrianbucks <adrianbucks@gmail.com>
2019-10-23 10:27:25 +0100

    Create 4LIFE.sol file
    initial comit

---
+++ 4LIFE.sol
@@ -0,0 +1,50 @@
+pragma solidity ^0.5.0;
+
+/**
+ * @dev Contract module which provides a basic access control mechanism, where
+ * there is an account (an owner) that can be granted exclusive access to
+ * specific functions.
+ *
+ * This module is used through inheritance. It will make available the modifier
+ * `onlyOwner`, which can be applied to your functions to restrict their use to
+ * the owner.
+ */
+ 
+ Contract Ownable {
+     address public owner;
+     
+     event TransferOwnership(address _from, address _to);
+     
+     /**
+     * @dev Initializes the contract setting the deployer as the initial owner.
+     */
+     constructor() public {
+         owner = msg.sender;
+         emit TransferOwnership(address(0), msg.sender);
+     }
+     
+     /**
+     * @dev Throws if called by any account other than the owner.
+     */
+     modifier onlyOwner() {
+         require(msg.sender == owner, "Ownable: caller is not the owner");
+         _;
+     }
+     
+     /**
+     * @dev Returns true if the caller is the current owner.
+     */
+     function isOwner() public view returns (bool) {
+         return msg.sender == owner;
+     }
+     
+     /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Can only be called by the current owner.
+     */
+     function TransferOwnership(address _newOwner) external onlyOwner {
+         require(newOwner != address(0), "Ownable: new owner is the zero address");
+         emit TransferOwnership(owner, _newOwner);
+         owner = _newOwner;
+     }
+ }
