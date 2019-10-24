fd2d747d0f532c5ffcd3537e9ca4e415629e0dd8
adrianbucks <adrianbucks@gmail.com>
2019-10-24 12:41:28 +0100

    new

--- 4LIFE.sol
+++ 4LIFE.sol
@@ -9,42 +9,108 @@ pragma solidity ^0.5.0;
  * `onlyOwner`, which can be applied to your functions to restrict their use to
  * the owner.
  */
- 
+
 contract Ownable {
-     address public owner;
-     
-     event OwnershipTransferred(address _from, address _to);
-     
-     /**
+    address public owner;
+
+    event OwnershipTransferred(address _from, address _to);
+
+    /**
      * @dev Initializes the contract setting the deployer as the initial owner.
      */
-     constructor() public {
-         owner = msg.sender;
-         emit OwnershipTransferred(address(0), msg.sender);
-     }
-     
-     /**
+    constructor() public {
+        owner = msg.sender;
+        emit OwnershipTransferred(address(0), msg.sender);
+    }
+
+    /**
      * @dev Throws if called by any account other than the owner.
      */
-     modifier onlyOwner() {
-         require(msg.sender == owner, "Ownable: caller is not the owner");
-         _;
-     }
-     
-     /**
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Ownable: caller is not the owner");
+        _;
+    }
+
+    /**
      * @dev Returns true if the caller is the current owner.
      */
-     function isOwner() public view returns (bool) {
-         return msg.sender == owner;
-     }
-     
-     /**
+    function isOwner() public view returns (bool) {
+        return msg.sender == owner;
+    }
+
+    /**
      * @dev Transfers ownership of the contract to a new account (`newOwner`).
      * Can only be called by the current owner.
      */
-     function TransferOwnership(address _newOwner) external onlyOwner {
-         require(_newOwner != address(0), "Ownable: new owner is the zero address");
-         emit OwnershipTransferred(owner, _newOwner);
-         owner = _newOwner;
-     }
- }
+    function TransferOwnership(address _newOwner) external onlyOwner {
+        require(_newOwner != address(0), "Ownable: new owner is the zero address");
+        emit OwnershipTransferred(owner, _newOwner);
+        owner = _newOwner;
+    }
+}
+
+/**
+ * @dev Wrappers over Solidity's arithmetic operations with added overflow
+ * checks.
+ */
+
+library SafeMath {
+    /**
+     * @dev Returns the addition of two unsigned integers, reverting on overflow.
+     */
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        require(c >= a, "SafeMath: addition overflow");
+
+        return c;
+    }
+
+    /**
+     * @dev Returns the subtraction of two unsigned integers, reverting on overflow (when the result is negative).
+     */
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b <= a, "SafeMath: subtraction overflow");
+        uint256 c = a - b;
+
+        return c;
+    }
+
+    /**
+     * @dev Returns the multiplication of two unsigned integers, reverting on overflow.
+     */
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        if (a == 0) {
+            return 0;
+        }
+
+        uint256 c = a * b;
+        require(c / a == b, "SafeMath: multiplication overflow");
+
+        return c;
+    }
+
+    /**
+     * @dev Returns the division of two unsigned integers, reverting on division by zero.
+     * The result is rounded towards zero.
+     */
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b > 0, "SafeMath: division by zero");
+        uint256 c = a / b;
+
+        return c;
+    }
+
+    /**
+     * @dev Returns the division of two unsigned integers, reverting on division by zero.
+     * The result is rounded towards infinity.
+     */
+    function divRound(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b > 0, "SafeMath: division by zero");
+        uint256 c = a / b;
+        if (a % b != 0) {
+            c = c + 1;
+        }
+
+        return c;
+    }
+}
