e68b6205bb764cb5d1c29f4aa31e6859ccea44ea
adrianbucks <adrianbucks@gmail.com>
2019-10-29 08:44:30 +0000

    new

--- 4LIFE.sol
+++ 4LIFE.sol
@@ -134,7 +134,7 @@ library SafeMath {
      * `b` must be an integer
      */
     function percVal(uint256 a, uint256 b) internal pure returns (uint256) {
-        uint256 c = mul(a, b) / 100;
+        uint256 c = a.mul(b).div(100);
 
         return c;
     }
@@ -183,4 +183,189 @@ interface IERC20 {
      * @dev Emitted when the allowance of a `spender` for an `owner` is set by a call to {approve}. `value` is the new allowance.
      */
     event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+/**
+ * @dev Optional functions from the ERC20 standard.
+ */
+contract ERC20Detailed is IERC20 {
+    string private _name;
+    string private _symbol;
+    uint8 private _decimals;
+
+    /**
+     * @dev Sets the values for `name`, `symbol`, and `decimals`.
+     */
+    constructor (string memory name, string memory symbol, uint8 decimals) public {
+        _name = name;
+        _symbol = symbol;
+        _decimals = decimals;
+    }
+
+    /**
+     * @dev Returns the name of the token.
+     */
+    function name() public view returns (string memory) {
+        return _name;
+    }
+
+    /**
+     * @dev Returns the symbol of the token, usually a shorter version of the name
+     */
+    function symbol() public view returns (string memory) {
+        return _symbol;
+    }
+
+    /**
+     * @dev Returns the number of decimals used to get its user representation.
+     */
+    function decimals() public view returns (uint8) {
+        return _decimals;
+    }
+}
+
+/**
+ * @dev 4LIFE Token
+ */
+contract ForLIFEToken is Ownable, ERC20Detailed {
+
+    using SafeMath for uint256;
+
+    /**
+     * Token metadata
+     */
+    string private _tokenName = "4LIFE";
+    string private _tokenSymbol = "4LIFE";
+    uint8 private _tokenDecimals = 8;
+
+    // TO-DO    -    add the main wallet address
+    address payable pond;
+
+    /**
+     * Token management
+     */
+    mapping (address => uint256) private _balances;
+    mapping (address => mapping (address => uint256)) private _allowed;
+    uint256 private _totalSupply = 2000000.mul(10**_tokenDecimals);
+    uint256 private _percent = 2;
+
+    /**
+     * Events
+     */
+    event SetPond(address indexed previousOwner, address indexed newOwner);
+
+    /**
+     * @dev Create the token
+     */
+    constructor () public payable ERC20Detailed(_tokenName, _tokenSymbol, _tokenDecimals) {
+        _balances[pond] = _totalSupply;
+
+        emit Transfer(address(0), pond, _totalSupply);
+    }
+
+    /**
+     * @dev Public methods
+     */
+    function getPond() public view returns (address) {
+
+        return pond;
+    }
+
+    function setPond(address payable pondAddress) public onlyOwner {
+        _setPond(pondAddress);
+    }
+
+    function _setPond(address _pondAddress) internal {
+        require(_pondAddress != address(0), "4LIFE: Pond address is address(0)");
+        emit SetPond(address(0), _pondAddress);
+        pond = _pondAddress;
+    }
+
+    function totalSupply() public view returns (uint256) {
+
+        return _totalSupply;
+    }
+
+    function balanceOf(address account) public view returns (uint256) {
+
+        return _balances[account];
+    }
+
+    function allowance(address account, address spender) public view returns (uint256) {
+
+        return _allowed[account][spender];
+    }
+
+    function transfer(address to, uint256 amount) public returns (bool) {
+        require(amount <= _balances[msg.sender], "4LIFE: Not enough funds");
+        require(to != address(0), "4LIFE: Transfer to address(0)");
+
+        uint256 tokensToBurn = amount.percVal(_percent);
+        uint256 tokensToPond = tokensToBurn;
+        uint256 tokensToTransfer = amount.sub(tokensToBurn).sub(tokensToPond);
+
+        _balances[msg.sender] = _balances[msg.sender].sub(amount);
+        _balances[to] = _balances[to].add(tokensToTransfer);
+        _balances[pond] = _balances[pond].add(tokensToPond);
+        _totalSupply = _totalSupply.sub(tokensToBurn);
+
+        emit Transfer(msg.sender, to, tokensToTransfer);
+        emit Transfer(msg.sender, pond, tokensToPond);
+        emit Transfer(msg.sender, address(0), tokensToBurn);
+
+        return true;
+    }
+
+    function approve(address spender, uint256 amount) public returns (bool) {
+        require(spender != address(0), "4LIFE: Approve for address(0)");
+
+        _allowed[msg.sender][spender] = amount;
+
+        emit Approval(msg.sender, spender, amount);
+
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint256 amount) public returns (bool) {
+        require(amount <= _balances[from], "4LIFE: Not enough funds");
+        require(amount <= _allowed[from][msg.sender], "4LIFE: Not enough allowance");
+        require(to != address(0), "4LIFE: Transfer to address(0)");
+
+        uint256 tokensToBurn = amount.percVal(_percent);
+        uint256 tokensToPond = tokensToBurn;
+        uint256 tokensToTransfer = amount.sub(tokensToBurn).sub(tokensToPond);
+
+        _balances[from] = _balances[from].sub(amount);
+        _balances[to] = _balances[to].add(tokensToTransfer);
+        _balances[pond] = _balances[pond].add(tokensToPond);
+        _totalSupply = _totalSupply.sub(tokensToBurn);
+
+        _allowed[from][msg.sender] = _allowed[from][msg.sender].sub(amount);
+
+        emit Transfer(from, to, tokensToTransfer);
+        emit Transfer(from, pond, tokensToPond);
+        emit Transfer(from, address(0), tokensToBurn);
+
+        return true;
+    }
+
+    function increaseAllowance(address spender, uint256 addedValue) public returns (bool) {
+        require(spender != address(0), "4LIFE: Increase allowance to address(0)");
+
+        _allowed[msg.sender][spender] = _allowed[msg.sender][spender].add(addedValue);
+
+        emit Approval(msg.sender, spender, _allowed[msg.sender][spender]);
+
+        return true;
+    }
+
+    function decreaseAllowance(address spender, uint256 subtractedValue) public returns (bool) {
+        require(spender != address(0), "4LIFE: Decrease allowance to address(0)");
+
+        _allowed[msg.sender][spender] = (_allowed[msg.sender][spender].sub(subtractedValue));
+
+        emit Approval(msg.sender, spender, _allowed[msg.sender][spender]);
+
+        return true;
+    }
 }
